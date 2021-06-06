7ca2b8030f5883516e1e9bbc6b7270a7f3bedd40
ErenYaylaci <ErenYaylaci@DESKTOP-QUJIPHN>
2021-06-06 12:56:10 +0300

    finished commit

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.set("view engine","ejs")
+app.set("view engine","ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 let items = [];
 
